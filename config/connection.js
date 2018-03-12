ae75e1aa71378902ca875fa552f63f1651c68180
Ryan Evans <Fatkid@Ryans-MacBook-Pro.local>
2018-03-12 14:51:21 -0700

    connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,6 @@ var mysql = require("mysql");
 var env = require("dotenv").config();
 
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-
-// var connection = mysql.createConnection({
 //   port: 3306,
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER,
@@ -21,12 +9,12 @@ connection.end();
 //   database: "burger_db"
 // });
 
-// connection.connect(function(err){
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
+connection.connect(function(err){
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
