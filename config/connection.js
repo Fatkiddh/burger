79bd66e86cff88bb46b98a121a073298ead2a657
Ryan Evans <Fatkid@Ryans-MacBook-Pro.local>
2018-03-12 13:54:17 -0700

    deploying with sql

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,32 @@
 var mysql = require("mysql");
 var env = require("dotenv").config();
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burger_db"
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
 });
 
-connection.connect(function(err){
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.end();
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: "burger_db"
+// });
+
+// connection.connect(function(err){
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 module.exports = connection;
