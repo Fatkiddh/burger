56bc221f69fa4a1a871f826af28fa03654097d43
Ryan Evans <Fatkid@ryans-mbp.lan>
2019-03-27 16:56:18 -0700

    fixed sql issues

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
 var mysql = require("mysql");
 var env = require("dotenv").config();
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
 //   port: 3306,
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER,
 //   password: process.env.DB_PASS,
 //   database: "burger_db"
 // });
+
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+  
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: "burger_db"
+  });
+}
 
 connection.connect(function(err){
   if (err) {
